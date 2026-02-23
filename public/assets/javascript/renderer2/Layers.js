94d509b3acaafa90285d5657b2fd9d391d7df7fa
jepidoptera <jepidoptera@live.com>
2026-02-22 22:12:49 -0600

    layers.js

---
+++ public/assets/javascript/renderer2/Layers.js
@@ -0,0 +1,35 @@
+(function attachRenderer2Layers(global) {
+    class Renderer2Layers {
+        constructor() {
+            this.root = new PIXI.Container();
+            this.root.name = "renderer2Root";
+
+            this.ground = new PIXI.Container();
+            this.ground.name = "renderer2Ground";
+
+            this.roadsFloor = new PIXI.Container();
+            this.roadsFloor.name = "renderer2RoadsFloor";
+
+            this.depthObjects = new PIXI.Container();
+            this.depthObjects.name = "renderer2DepthObjects";
+
+            this.objects3d = new PIXI.Container();
+            this.objects3d.name = "renderer2Objects3d";
+
+            this.entities = new PIXI.Container();
+            this.entities.name = "renderer2Entities";
+
+            this.ui = new PIXI.Container();
+            this.ui.name = "renderer2Ui";
+
+            this.root.addChild(this.ground);
+            this.root.addChild(this.roadsFloor);
+            this.root.addChild(this.depthObjects);
+            this.root.addChild(this.objects3d);
+            this.root.addChild(this.entities);
+            this.root.addChild(this.ui);
+        }
+    }
+
+    global.Renderer2Layers = Renderer2Layers;
+})(typeof globalThis !== "undefined" ? globalThis : window);
